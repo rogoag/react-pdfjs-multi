0d9fa9de508b634a442356de40f5654af66a7d96
Marcus Klein <marcus.klein@live.com>
2018-09-21 22:47:10 +0200

    clean up

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
-import babel from 'rollup-plugin-babel';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
-import url from 'rollup-plugin-url';
 import postcssUrl from 'postcss-url';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
@@ -12,17 +10,13 @@ export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/index.js',
+      file: pkg.main,
       format: 'cjs',
     },
     {
-      file: 'dist/index.es.js',
+      file: pkg.module,
       format: 'es',
     },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
   ],
   plugins: [
     postcss({
@@ -30,7 +24,6 @@ export default {
       extract: 'dist/react-pdfjs-multi.css',
     }),
     external(),
-    url(),
     typescript({
       typescript: require('typescript'),
     }),
